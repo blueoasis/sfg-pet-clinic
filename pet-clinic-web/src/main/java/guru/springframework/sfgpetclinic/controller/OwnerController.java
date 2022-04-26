862f8da7cf336840ac3a560114993abe35e7d9c2
blueoasis <blueoasis@users.noreply.github.com>
2022-04-25 22:17:37 -0600

    list owners and vets. closes #15. closes #16.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
@@ -1,13 +1,22 @@
 package guru.springframework.sfgpetclinic.controller;
 
+import guru.springframework.sfgpetclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
-    
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/owners","/owners/index", "/owners/index.html"})
-    public String owner() {
+    public String owners(Model model) {
+        model.addAttribute("owners",this.ownerService.findAll());
         return "owners/index";
     }
 }
