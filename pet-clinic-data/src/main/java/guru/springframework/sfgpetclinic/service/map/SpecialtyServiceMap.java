59e683ae580ef0b0dccc2964f0ff40d0cfcbd4c2
blueoasis <blueoasis@users.noreply.github.com>
2022-04-28 14:49:42 -0600

    Create Specialty Service. Closes #28

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/SpecialtyServiceMap.java
@@ -0,0 +1,35 @@
+package guru.springframework.sfgpetclinic.service.map;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+import guru.springframework.sfgpetclinic.service.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Specialty,Long> implements SpecialtyService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
